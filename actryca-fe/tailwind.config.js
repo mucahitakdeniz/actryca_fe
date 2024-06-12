34d37e238224c6a7cab26653eff2c587bbc91a21
YildizGokhan <gygokhanyildiz1@gmail.com>
2024-06-12 21:06:22 +0300

    tailwind and package.json added.

--- actryca-fe/tailwind.config.js
+++ actryca-fe/tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "Primary-900": "#231b32",
+        "Primary-800": "#322748",
+        "Primary-600": "#513f75",
+        "Primary-500": "#614B8B",
+        "Primary-100": "#C6BED5" ,
+        "Primary-50": "#E3DAF3",
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+        'dm-serif-text': ['DM Serif Text', 'serif'],
+      },
     },
   },
   plugins: [],
