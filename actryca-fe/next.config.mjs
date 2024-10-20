26fbb483b6de19b9f7ccd0dcc9e10b543741ee66
Yasin Türk <yasinturk03@gmail.com>
2024-10-20 14:32:36 +0300

    fix: next-config

--- actryca-fe/next.config.mjs
+++ actryca-fe/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://3.120.147.238:8000/:path*", // HTTP proxy
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
